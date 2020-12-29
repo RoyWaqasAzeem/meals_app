cd5a418a458eedb37c398028647915b1a671b9ae
Roy123 <bsef15m046@pucit.edu.pk>
2020-12-29 16:50:33 +0500

    Futter Ios and Android meals app

--- lib/main.dart
+++ lib/main.dart
@@ -70,6 +70,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Deli Meals',
       theme: ThemeData(
         primarySwatch: Colors.pink,
